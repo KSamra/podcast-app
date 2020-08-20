fea5f5a030247ab6eda1dc3ebaad33385b94210d
Kavan Samra <kavansamra@gmail.com>
2020-08-20 15:02:54 -0700

    working on search api

--- pages/api/search.js
+++ pages/api/search.js
@@ -1,6 +1,38 @@
+const FeedParser = require('feedparser');
+const fetch = require('node-fetch')
 
+const iTunesUrl = 'https://itunes.apple.com/search?entity=podcast'
+const feedparser = new FeedParser();
 
-export default function handler(req, res) {
-  console.log(req.body)
-  return res.status(200).send({data: 'good request'})
+feedparser.on('readable', function() {
+  let stream = this;
+  let meta = this.meta;
+
+  let item;
+
+  while(item = stream.read()){
+    console.dir(item, {depth: 0})
+  }
+})
+
+export default async function handler(req, res) {
+  
+  let {key,limit=25} = req.query;
+  console.log('received search key = ', key, ' with limit ', limit);
+  try {
+    const response = await fetch(`${iTunesUrl}&term=${key}&limit=${limit}`);
+    const results = await response.json();
+
+    const podcastFeed = await fetch(results.results.feedUrl)
+    console.log(podcastFeed.body)
+
+    podcastFeed.body.pipe(feedparser)
+
+    return res.status(200).send({data: results.results[0]})
+  } catch (error) {
+    console.log(error)
+    return res.status(500).send({error})
+  }
+  
+  
 }
