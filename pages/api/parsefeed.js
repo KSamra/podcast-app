e2eb3406f4ae26462b13ecef836dc9092c276e55
Kavan Samra <kavansamra@gmail.com>
2020-08-21 16:52:19 -0700

    print out feed stream. Just a simple example, need to actually prepare and transform the response

--- pages/api/parsefeed.js
+++ pages/api/parsefeed.js
@@ -1,27 +1,35 @@
 const FeedParser = require('feedparser');
 const fetch = require('node-fetch');
 
-const feedparser = new FeedParser();
-feedparser.on('error', function(error){
-  console.error(error);
-})
 
-feedparser.on('readable', function() {
-  let stream = this;
-  let item;
-  while(item = stream.read()){
-    console.dir(item, {depth: 0})
-  }
-})
 
 
 export default async function handler(req, res) {
+  const feedparser = new FeedParser();
+  // feedparser.on('error', function(error){
+  //   console.error(error);
+  //   res.send(error)
+  // })
+
+  // feedparser.on('readable', function() {
+  //   let stream = this;
+  //   let item;
+  //   while(item = stream.read()){
+  //     let {title, enclosures, author, date, link, origlink} = item
+  //     let episode = item['itunes:episode']
+  //     let pod = [title, author, date, enclosures, link, origlink, episode]
+  //     feedparser.write(res)
+  //   }
+  //   res.status(200).send({status: "all good"})
+    
+  // })
   console.log(req.body)
   let {feedUrl} = req.body;
 
   const request = await fetch(feedUrl);
-  request.body.pipe(feedparser)
+  // res.write('sending data...')
+  request.body.pipe(feedparser).pipe(res)
 
-  res.status(200).send({msg: 'successful!'})
+  // res.status(200).send({msg: 'successful!'})
 
 }
