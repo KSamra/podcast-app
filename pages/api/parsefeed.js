23fd10ca02d09a455242876b1357a79b3f29e6d4
Kavan Samra <kavansamra@gmail.com>
2020-09-22 14:36:40 -0700

    cleaned up parsefeed file

--- pages/api/parsefeed.js
+++ pages/api/parsefeed.js
@@ -4,6 +4,36 @@ const fetch = require('node-fetch');
 const { Transform } = require('stream');
 
 
+const jsonTransformer = new Transform({
+  writableObjectMode: true,
+  transform(chunk, encoding, callback) {
+    try {
+      if(chunk === null){
+        this.push(null);
+      }
+
+      let {title, enclosures, author, date, link, origlink} = chunk
+      let episode = chunk['itunes:episode']
+      let pod = JSON.stringify({title, author, date, enclosures, link, origlink, episode})
+      this.push(pod)
+      callback();
+
+    } catch (error) {
+      this.push(null);
+      callback(error);
+    }
+  }
+})
+
+jsonTransformer.on('error', () => {
+  console.log('JsonTransformer errored or finished!')
+  res.end();
+});
+
+jsonTransformer.on('finish', () => {
+  console.log(`JSONTransformer finished it's transformation!`);
+})
+
 
 export default async function handler(req, res) {
   console.log(req.body)
@@ -14,32 +44,6 @@ export default async function handler(req, res) {
   let {feedUrl} = req.body;
   const request = await fetch(feedUrl);
   
-  const jsonTransformer = new Transform({
-    writableObjectMode: true,
-    transform(chunk, encoding, callback) {
-      try {
-        if(chunk === null){
-          this.push(null);
-        }
-  
-        let {title, enclosures, author, date, link, origlink} = chunk
-        let episode = chunk['itunes:episode']
-        let pod = {title, author, date, enclosures, link, origlink, episode}
-        let j = JSON.stringify(pod);
-        this.push(j)
-        callback();
-  
-      } catch (error) {
-        this.push(null);
-        callback(error);
-      }
-    }
-  })
-  
-  jsonTransformer.on('error', () => {
-    console.log('JsonTransformer errored or finished!')
-    res.end()
-  });
   
 
   const feedparser = new FeedParser();
